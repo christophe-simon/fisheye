025c1929277fac0df89bd343f129bbfe431a72ae
Christophe Simon <csimon.developpeur@gmail.com>
2022-06-07 01:02:14 +0200

    Update index.html

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,51 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, id, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
+    const place = `${city}, ${country}`;
+    const dailyPrice = `${price}€/jour`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
+        const articleElt = document.createElement('article');
+        articleElt.classList.add('photographer_card');
+
+        const linkElt = document.createElement('a');
+        linkElt.classList.add('photographer_card__link');
+        linkElt.href = `photographer.html?photographer=${id}`;
+
+        const imgElt = document.createElement('img');
+        imgElt.setAttribute('src', picture);
+        imgElt.setAttribute('alt', name);
+
+        const h2Elt = document.createElement('h2');
+        h2Elt.textContent = name;
+
+        const divElt = document.createElement('div');
+        divElt.classList.add('photographer_card__description');
+
+        const placeElt = document.createElement('p');
+        placeElt.classList.add('place');
+        placeElt.textContent = place;
+
+        const taglineElt = document.createElement('p');
+        taglineElt.classList.add('tagline');
+        taglineElt.textContent = tagline;
+
+        const priceElt = document.createElement('p');
+        priceElt.classList.add('price');
+        priceElt.textContent = dailyPrice;
+
+        linkElt.appendChild(imgElt);
+        linkElt.appendChild(h2Elt);
+
+        divElt.appendChild(placeElt);
+        divElt.appendChild(taglineElt);
+        divElt.appendChild(priceElt);
+
+        articleElt.appendChild(linkElt);
+        articleElt.appendChild(divElt);
+
+        return (articleElt);
     }
     return { name, picture, getUserCardDOM }
 }
